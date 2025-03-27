b2e793d606562752b73fb5ff758d9d5f6499163c
mariankrotil <mariankrotil34@gmail.com>
2025-03-27 11:10:46 +0100

    KAB-871 feat: update job tools, add new tool to list jobs given component

--- src/keboola_mcp_server/jobs_tools.py
+++ src/keboola_mcp_server/jobs_tools.py
@@ -18,9 +18,15 @@ class JobListItem(BaseModel):
     status: Literal["waiting", "processing", "success", "error"] = Field(
         description="The status of the job."
     )
-    created_time: str = Field(description="The creation time of the job.", alias="createdTime")
-    start_time: str = Field(description="The start time of the job.", alias="startTime")
-    end_time: str = Field(description="The end time of the job.", alias="endTime")
+    created_time: Optional[str] = Field(
+        description="The creation time of the job.", alias="createdTime", default=None
+    )
+    start_time: Optional[str] = Field(
+        description="The start time of the job.", alias="startTime", default=None
+    )
+    end_time: Optional[str] = Field(
+        description="The end time of the job.", alias="endTime", default=None
+    )
 
 
 class JobDetail(JobListItem):
@@ -45,6 +51,8 @@ def add_jobs_tools(mcp: FastMCP) -> None:
     """Add tools to the MCP server."""
     mcp.add_tool(list_jobs)
     mcp.add_tool(get_job_details)
+    mcp.add_tool(list_component_config_jobs)
+    mcp.add_tool(list_component_jobs)
     logger.info("Jobs tools added to the MCP server.")
 
 
@@ -66,3 +74,48 @@ async def get_job_details(job_id: Annotated[str, "The ID of the job."], ctx: Con
     r_job = client.storage_client.jobs.detail(job_id)
     logger.info(f"Found job details for {job_id}." if r_job else f"Job {job_id} not found.")
     return JobDetail.model_validate(r_job)
+
+
+def filter_by_config_id(job: JobDetail, config_id: str) -> bool:
+    return job.operation_params and job.operation_params.get("configurationId", None) == config_id
+
+
+def filter_by_component_id(job: JobDetail, component_id: str) -> bool:
+    return job.operation_params and job.operation_params.get("componentId", None) == component_id
+
+
+async def list_component_config_jobs(
+    component_id: str, config_id: str, n_limit: Optional[int], ctx: Context
+) -> List[JobListItem]:
+    """List all jobs for a given component id and configuration id, optionally limited listsize by the n_limit parameter."""
+    client = ctx.session.state["sapi_client"]
+    assert isinstance(client, KeboolaClient)
+
+    r_jobs = client.storage_client.jobs.list()
+    jobs = [JobDetail.model_validate(r_job) for r_job in r_jobs]
+    jobs = filter(
+        lambda j: filter_by_config_id(j, config_id) and filter_by_component_id(j, component_id),
+        jobs,
+    )
+    jobs = list(jobs)
+    # Convert JobDetail to JobListItem
+    n_limit = n_limit or len(jobs)
+    n_limit = max(n_limit, 1)
+    return [JobListItem.model_validate(j.model_dump()) for j in jobs[:n_limit]]
+
+
+async def list_component_jobs(
+    component_id: str, n_limit: Optional[int], ctx: Context
+) -> List[JobListItem]:
+    """List all jobs for a given component id, optionally limited list size by the n_limit parameter."""
+    client = ctx.session.state["sapi_client"]
+    assert isinstance(client, KeboolaClient)
+
+    r_jobs = client.storage_client.jobs.list()
+    jobs = [JobDetail.model_validate(r_job) for r_job in r_jobs]
+    jobs = filter(lambda j: filter_by_component_id(j, component_id), jobs)
+    jobs = list(jobs)
+    # Convert JobDetail to JobListItem
+    n_limit = n_limit or len(jobs)
+    n_limit = max(n_limit, 1)
+    return [JobListItem.model_validate(j.model_dump()) for j in jobs[:n_limit]]
