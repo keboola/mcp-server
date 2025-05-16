1968f990289a52f916b4a7feb156ab0a36baedbc
mariankrotil <mariankrotil34@gmail.com>
2025-05-16 16:25:27 +0200

    KAB-998 refactor: remove inheritance and method overriding from mcp

--- src/keboola_mcp_server/mcp.py
+++ src/keboola_mcp_server/mcp.py
@@ -24,123 +24,18 @@ Issues:
 
 import logging
 import textwrap
-from contextlib import AsyncExitStack
-from typing import Any, Callable, Optional
+from typing import Any, Optional
 
-import anyio
-from anyio.streams.memory import MemoryObjectReceiveStream, MemoryObjectSendStream
 from fastmcp import FastMCP
-from mcp import ServerSession
-from mcp.server.lowlevel.server import Server
-from mcp.server.models import InitializationOptions
-from mcp.shared.message import SessionMessage
 from mcp.types import AnyFunction, ToolAnnotations
+
+from keboola_mcp_server.config import Config
 
 LOG = logging.getLogger(__name__)
 
-SessionParams = dict[str, str]
-SessionState = dict[str, Any]
-SessionStateFactory = Callable[[Optional[SessionParams]], SessionState]
-
-
-class StatefulServerSession(ServerSession):
-    def __init__(
-        self,
-        read_stream: MemoryObjectReceiveStream[SessionMessage | Exception],
-        write_stream: MemoryObjectSendStream[SessionMessage],
-        init_options: InitializationOptions,
-        stateless: bool = False,
-        state: SessionState | None = None,
-        **kwargs: Any,
-    ) -> None:
-        if stateless:
-            LOG.warning(
-                'Stateless mode is not supported for StatefulServerSession, stateless parameter will be ignored.'
-            )
-        super().__init__(read_stream, write_stream, init_options, stateless=False, **kwargs)
-        self._state = state or {}
-
-    @property
-    def state(self) -> SessionState:
-        return self._state
-
-
-def _default_session_state_factory() -> SessionStateFactory:
-    def _(params: SessionParams | None = None) -> SessionState:
-        return params or {}
-
-    return _
-
-
-class _KeboolaServer(Server):
-
-    def __init__(self, session_state_factory: SessionStateFactory | None = None, *args: Any, **kwargs: Any) -> None:
-        super().__init__(*args, **kwargs)
-        self._session_state_factory = session_state_factory or _default_session_state_factory()
-
-    async def run(
-        self,
-        read_stream: MemoryObjectReceiveStream[SessionMessage | Exception],
-        write_stream: MemoryObjectSendStream[SessionMessage],
-        initialization_options: InitializationOptions,
-        # When False, exceptions are returned as messages to the client.
-        # When True, exceptions are raised, which will cause the server to shut down
-        # but also make tracing exceptions much easier during testing and when using
-        # in-process servers.
-        raise_exceptions: bool = False,
-        # When True, the server is stateless and
-        # clients can perform initialization with any node. The client must still follow
-        # the initialization lifecycle, but can do so with any available node
-        # rather than requiring initialization for each connection.
-        stateless: bool = False,
-        **kwargs: Any,
-    ):
-        """
-        This method is overridden to allow passing the session state factory to the session.
-        Other approach would be use the session in appropriate place in the code (in tools, etc.), but this
-        approach allows to have the session state factory in the server constructor and use it in the tools.
-        """
-        async with AsyncExitStack() as stack:
-            lifespan_context = await stack.enter_async_context(self.lifespan(self))
-            session = await stack.enter_async_context(
-                StatefulServerSession(
-                    read_stream,
-                    write_stream,
-                    initialization_options,
-                    stateless=stateless,
-                    state=self._session_state_factory(None),
-                    **kwargs,
-                )
-            )
-
-            async with anyio.create_task_group() as tg:
-                async for message in session.incoming_messages:
-                    LOG.debug(f'Received message: {message}')
-
-                    tg.start_soon(
-                        self._handle_message,
-                        message,
-                        session,
-                        lifespan_context,
-                        raise_exceptions,
-                    )
-
 
 class KeboolaMcpServer(FastMCP):
-    def __init__(
-        self,
-        session_state_factory: SessionStateFactory | None = None,
-        *args: Any,
-        **settings: Any,
-    ) -> None:
-        super().__init__(*args, **settings)
-        self._mcp_server = _KeboolaServer(
-            name=self._mcp_server.name,
-            instructions=self._mcp_server.instructions,
-            lifespan=self._mcp_server.lifespan,
-            session_state_factory=session_state_factory,
-        )
-        self._setup_handlers()
+    config: Config | None = None
 
     def add_tool(
         self,
@@ -157,3 +52,6 @@ class KeboolaMcpServer(FastMCP):
             tags=tags,
             annotations=annotations,
         )
+
+    def set_config(self, config: Optional[Config] = None) -> None:
+        self.config = config
