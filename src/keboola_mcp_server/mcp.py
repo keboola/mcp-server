6b2024cf2c86fd468a491ae2be8c237bf1a4a87b
mariankrotil <mariankrotil34@gmail.com>
2025-05-20 15:22:03 +0200

    KAB-998 feat: update session_state decorator to use server lifespan

--- src/keboola_mcp_server/mcp.py
+++ src/keboola_mcp_server/mcp.py
@@ -24,14 +24,14 @@ import inspect
 import logging
 import os
 import textwrap
+from dataclasses import asdict, dataclass
 from functools import wraps
-from typing import Any, Callable, Literal, Optional
+from typing import Any, Callable, Literal, Optional, cast
 
 from fastmcp import Context, FastMCP
 from fastmcp.server.dependencies import get_http_request
 from fastmcp.utilities.types import find_kwarg_by_type
 from mcp.types import AnyFunction, ToolAnnotations
-from starlette.requests import Request
 
 from keboola_mcp_server.client import KeboolaClient
 from keboola_mcp_server.config import Config
@@ -43,10 +43,23 @@ TransportType = Literal['stdio', 'sse', 'streamable-http']
 SessionParams = dict[str, str]
 SessionState = dict[str, Any]
 SessionStateFactory = Callable[[Optional[SessionParams]], SessionState]
+SessionParamsFactory = Callable[[Optional[Context]], SessionParams]
+SessionStateInjector = Callable[[str, SessionStateFactory, SessionParamsFactory], AnyFunction]
+
+
+@dataclass
+class ServerState:
+    config: Config
+
+    @classmethod
+    def from_context(cls, ctx: Context) -> 'ServerState':
+        life_span = ctx.request_context.lifespan_context
+        if not isinstance(life_span, ServerState):
+            raise ValueError('ServerState is not available in the context.')
+        return life_span
 
 
 class KeboolaMcpServer(FastMCP):
-    config: Config | None = None
 
     def add_tool(
         self,
@@ -64,16 +77,34 @@ class KeboolaMcpServer(FastMCP):
             annotations=annotations,
         )
 
-    def set_config(self, config: Optional[Config] = None) -> None:
-        self.config = config
+
+def keboola_session_params_factory(ctx: Optional[Context] = None) -> SessionParams:
+    """
+    Creates session parameters for the Keboola MCP server, expecting a context with a lifespan context.
+    :param ctx: The context of the tool function.
+    :returns: The session parameters, if not provided, falls back to the server configuration.
+    """
+    if ctx:
+        life_span = ServerState.from_context(ctx)
+        LOG.info(f'Creating SessionParams from server config: {life_span.config}.')
+        config = life_span.config
+        session_params = _get_session_params(cast(TransportType, config.transport))
+        return asdict(config.replace_by(session_params))
+    else:
+        LOG.info('Inferring SessionParams.')
+        session_params = _get_session_params(None)
+        return asdict(Config.from_dict(session_params))
 
 
-def session_state_factory(params: SessionParams | None = None) -> SessionState:
+def keboola_session_state_factory(params: SessionParams | None = None) -> SessionState:
+    """
+    Creates a session state for the Keboola MCP server, expecting session parameters.
+    :param params: The session parameters.
+    :returns: The session state.
+    """
     params = params or {}
     LOG.info(f'Creating SessionState for params: {params.keys() if params else "None"}.')
-
     cfg = Config.from_dict(params)
-
     LOG.info(f'Creating SessionState from config: {cfg}.')
 
     state: SessionState = {}
@@ -104,29 +135,33 @@ def session_state_factory(params: SessionParams | None = None) -> SessionState:
 
 def with_session_state(
     session_state_field_name: str = 'state',
-    session_state_factory: SessionStateFactory = session_state_factory
-):
+    session_state_factory: SessionStateFactory = keboola_session_state_factory,
+    session_params_factory: SessionParamsFactory = keboola_session_params_factory,
+) -> Any:
     """
     Simplified decorator to inject the session state into the session of the Context parameter of a tool function using
-    Keboola-specific session_state_factory in order not to have to specify the default decorator and
-    session_state_factory parameters all the time.
+    Keboola-specific session_state_factory and session_params_factory in order not to have to specify the default
+    values all the time.
     """
     return with_session_state_from(
         session_state_field_name=session_state_field_name,
         session_state_factory=session_state_factory,
-        custom_decorator=None,
+        session_params_factory=session_params_factory,
+        custom_session_state_injector=None,
     )
 
 
 def with_session_state_from(
     session_state_field_name: str = 'state',
     session_state_factory: SessionStateFactory | None = None,
-    custom_decorator: Callable[[str, SessionStateFactory], AnyFunction] | None = None,
-) -> Callable[[AnyFunction], AnyFunction]:
+    session_params_factory: SessionParamsFactory | None = None,
+    custom_session_state_injector: SessionStateInjector | None = None,
+) -> AnyFunction:
     """Decorator to inject the session state into the Context parameter of a tool function.
 
-    This decorator dynamically inserts a session state object into the Context parameter of a tool function.
-    The session state is created using the session_state_factory and attached to the Context's session.
+    This decorator dynamically inserts a session state object into the Context parameter of a tool function. Session
+    parameters are created using the session_params_factory and the session state is created using the
+    session_state_factory. The session state is then attached to the Context's session.
     This allows tools to access and modify persistent state across multiple requests.
 
     :param session_state_field_name: The name of the field in the Context.session object that will hold the
@@ -134,9 +169,11 @@ def with_session_state_from(
     :param session_state_factory: The factory function that creates the session state. If not provided, the
     default factory is used. The factory function can use the `get_context`, `get_http_request` or access the
     `environment` variables to create the session state.
-    :param custom_decorator: A custom decorator function that will be used to insert the session state into the
-    tool function. If not provided, the default decorator function will be used. To customize this,
-    follow the default_decorator pattern bellow.
+    :param session_params_factory: The factory function that creates the session parameters. If not provided, the
+    default factory is used. The factory function can use the `get_context` to create the session parameters.
+    :param custom_session_state_injector: A custom injector function that will be used to insert the session state
+    into the tool function. If not provided, the default injector function will be used. To customize this,
+    follow the _default_session_state_injector pattern bellow.
 
     example:
     ```python
@@ -152,12 +189,39 @@ def with_session_state_from(
             'Did you forget to call it? Use @session_state_from()'
         )
 
-    session_state_factory = session_state_factory or _default_session_state_factory()
+    session_state_factory = session_state_factory or default_session_state_factory()
+    session_params_factory = session_params_factory or default_session_params_factory()
+    injector = custom_session_state_injector or default_session_state_injector()
+    return injector(session_state_field_name, session_state_factory, session_params_factory)
 
-    def default_decorator(session_state_field_name: str, session_state_factory: SessionStateFactory) -> AnyFunction:
+
+def default_session_state_factory() -> SessionStateFactory:
+    def _(params: SessionParams | None = None) -> SessionState:
+        return params or {}
+
+    return _
+
+
+def default_session_params_factory() -> SessionParamsFactory:
+    def _(ctx: Optional[Context] = None) -> SessionParams:
+        return _get_session_params()
+
+    return _
+
+
+def default_session_state_injector() -> SessionStateInjector:
+    def _default_session_state_injector(
+        session_state_field_name: str,
+        session_state_factory: SessionStateFactory,
+        session_params_factory: SessionParamsFactory,
+    ) -> AnyFunction:
         """
-        Serves for wrapping the session_state_field_name and session_state_factory parameters due to customization
-        of the default decorator.
+        Wraps the session_state_field_name, session_state_factory and session_params_factory parameters due to
+        customization of the default decorator.
+        :param session_state_field_name: The name of the field in the Context.session object that will hold the
+        session state.
+        :param session_state_factory: The factory function that creates the session state.
+        :param session_params_factory: The factory function that creates the session parameters (from request...).
         """
 
         def _wrapper(fn: AnyFunction) -> AnyFunction:
@@ -172,8 +236,8 @@ def with_session_state_from(
                 has a parameter of a type Context, otherwise it raises an error. If the context already has the
                 session state, it is not overridden. It is executed by the MCP server when the annotated tool
                 function is called.
-
-                :param kwargs: The arguments of the tool function. Must include the Context type argument.
+                :param args: The positional arguments of the tool function.
+                :param kwargs: The keyword arguments of the tool function.
                 :raises TypeError: If the Context argument is not found in the function parameters.
                                     If the session instance is not available in the context.
                 :returns: The result of the tool function.
@@ -184,7 +248,6 @@ def with_session_state_from(
                     raise TypeError(
                         'Context argument is required, add "ctx: Context" parameter to the function parameters.'
                     )
-
                 # convert positional args to kwargs using inspect.signature in case context is passed as positional arg
                 updated_kwargs = inspect.signature(fn).bind(*args, **kwargs).arguments
                 ctx = updated_kwargs.get(ctx_kwarg) if ctx_kwarg else None
@@ -194,8 +257,7 @@ def with_session_state_from(
 
                 if not hasattr(ctx.session, session_state_field_name):
                     # get session params from the request or environment variables
-                    params: SessionParams = _get_session_params()
-                    # set the session state to the context session
+                    params: SessionParams = session_params_factory(ctx)
                     state: SessionState = session_state_factory(params)
                     setattr(ctx.session, session_state_field_name, state)
                 return await fn(*args, **kwargs)
@@ -204,62 +266,40 @@ def with_session_state_from(
 
         return _wrapper
 
-    decorator = custom_decorator or default_decorator
-    return decorator(session_state_field_name, session_state_factory)
+    return _default_session_state_injector
 
 
-def _default_session_state_factory() -> SessionStateFactory:
-    def _(params: SessionParams | None = None) -> SessionState:
-        return params or {}
-
-    return _
-
-
-def _safe_get_http_request() -> Optional[Request]:
+def _infer_session_params() -> SessionParams:
     """
-    Attempts to retrieve the current HTTP request. Returns None if not available.
+    Infers session parameters. If the request is available, session parameters are inferred from the request headers and
+    query params. Otherwise, it falls back to environment variables, since the server is running locally.
     """
     try:
-        return get_http_request()
+        request = get_http_request()
+        LOG.info('Inferring SessionParams from request.')
+        return dict(request.headers) | dict(request.query_params)
     except RuntimeError:
-        return None
-
-
-def _infer_session_params(request: Optional[Request] = None) -> SessionParams:
-    """
-    Infers session parameters from the current HTTP request if available, or falls back to environment variables.
-    :param request: The current HTTP request. If None, falls back to environment variables.
-    """
-    if request is None:
-        # Fallback to environment variables for stdio-based communication because there is no HTTP request
-        # So the server is running locally
+        LOG.info('Inferring SessionParams from environment variables.')
         return dict(os.environ)
-    if Config.contains_required_fields(request.query_params):
-        return dict(request.query_params)
-    elif Config.contains_required_fields(request.headers):
-        return dict(request.headers)
-    else:
-        LOG.warning('No required fields found in the request. Using empty session params.')
-        return {}
 
 
 def _get_session_params(transport: Optional[TransportType] = None) -> SessionParams:
     """
-    Gets or infers the session parameters based on the current HTTP request or environment variables from the given
-    transport.
-    :param transport: The transport type.
-        - 'stdio': Use environment variables.
-        - 'streamable-http': Use headers.
-        - 'sse': Use headers.
-        - None: Infer from the current HTTP request (query params or headers) or from environment variables.
+    Gets the session parameters from the given transport, or infers them from the current HTTP request or environment
+    variables if the transport is not specified.
+    :param transport: The transport type, if none, it is inferred. values: stdio, streamable-http, sse, None
     """
+    LOG.info(f'Retrieving SessionParams from transport: {transport}.')
+
+    if not transport:
+        return _infer_session_params()
+
     if transport == 'stdio':
         return dict(os.environ)
     if transport == 'streamable-http':
         request = get_http_request()
-        return dict(request.headers)
+        return dict(request.headers) | dict(request.query_params)
     if transport == 'sse':
         # SSE transport if explicitly specified, uses query params due to backward compatibility
         request = get_http_request()
         return dict(request.query_params)
-    return _infer_session_params(_safe_get_http_request())
