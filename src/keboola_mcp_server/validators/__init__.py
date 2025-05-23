c643d49a444301fa1b72309001c47944f6c019aa
mariankrotil <mariankrotil34@gmail.com>
2025-05-23 16:04:59 +0200

    KAB-1008 feat: add validators submodule

---
+++ src/keboola_mcp_server/validators/__init__.py
@@ -0,0 +1,6 @@
+"""
+Validators that validates the Agent generated configurations against the json schema or pydantic model. Since the
+corrupted configurations are not handled, and UI can crash, we need to validate the configurations before.
+"""
+
+from keboola_mcp_server.validators.validate import validate_storage_jsonschema, validate_storage_pydantic
