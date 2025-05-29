83a280704384edbac0cab2a22db0282490332f43
mariankrotil <mariankrotil34@gmail.com>
2025-05-29 18:10:18 +0200

    KAB-1008 test: update code wrt reviews

--- tests/tools/components/test_utils_validation.py
+++ tests/tools/components/test_utils_validation.py
@@ -4,7 +4,6 @@ from unittest.mock import AsyncMock, MagicMock
 import pytest
 
 from keboola_mcp_server.client import JsonDict, KeboolaClient
-from keboola_mcp_server.tools._validate import validate_parameters, validate_storage
 from keboola_mcp_server.tools.components import utils
 
 
@@ -20,11 +19,6 @@ from keboola_mcp_server.tools.components import utils
 )
 def test_validate_storage_configuration_output(input_storage: Optional[JsonDict], output_storage: Optional[JsonDict]):
     """testing normalized and returned structures {storage: {...}} vs {...}"""
-    if input_storage is not None and input_storage.get('storage') is not None:
-        result = validate_storage(input_storage)
-        expected = {'storage': output_storage}
-        assert result == expected  # we expect wrapped structure (normalized)
-
     result = utils.validate_storage_configuration(input_storage)
     expected = output_storage  # we expect unwrapped structure
     assert result == expected
@@ -41,12 +35,8 @@ def test_validate_storage_configuration_output(input_storage: Optional[JsonDict]
 async def test_validate_root_parameters_configuration_output(
     mocker, input_parameters: JsonDict, output_parameters: JsonDict, keboola_client: KeboolaClient
 ):
-    """testing normalized and returned structures {parameters: {...}} vs {...}"""
+    """testing returned format structures  {...}"""
     accepting_schema = {'type': 'object', 'additionalProperties': True}  # accepts any json object
-    result = validate_parameters(input_parameters, accepting_schema)
-    expected = {'parameters': output_parameters}
-    assert result == expected  # we expect wrapped structure (normalized)
-
     component = MagicMock()
     component.configuration_schema = accepting_schema
     mocker.patch('keboola_mcp_server.tools.components.utils._get_component', new=AsyncMock(return_value=component))
@@ -68,10 +58,6 @@ async def test_validate_row_parameters_configuration_output(
 ):
     """testing normalized and returned structures {parameters: {...}} vs {...}"""
     accepting_schema = {'type': 'object', 'additionalProperties': True}  # accepts any json object
-    result = validate_parameters(input_parameters, accepting_schema)
-    expected = {'parameters': output_parameters}
-    assert result == expected  # we expect wrapped structure (normalized)
-
     component = MagicMock()
     component.configuration_schema = accepting_schema
     mocker.patch('keboola_mcp_server.tools.components.utils._get_component', new=AsyncMock(return_value=component))
