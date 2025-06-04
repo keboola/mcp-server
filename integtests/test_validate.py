687c224195681fee3c8942633c72188e841dae8e
mariankrotil <mariankrotil34@gmail.com>
2025-06-04 15:00:32 +0200

    KAB-1008 test: update tests wrt reviews, add comments for tests

--- integtests/test_validate.py
+++ integtests/test_validate.py
@@ -1,3 +1,14 @@
+"""
+This test is used to validate both row and root parameter schemas of all components.
+- Serves as a sanity check for the schemas validation, identifying invalid schemas and proposing two possible solutions:
+    - Fix the json schema to be valid
+    - Improve the KeboolaParametersValidator to accept the schema
+- Ensures that all parameter schemas are valid and that the MCP server will them correctly to validate the parameters
+received from the LLM Agent.
+- In case the schema is invalid, we skip the validation, log the schema error but continue with the action (creation or
+update of the component) assuming that the validation of json object against the schema had been correct. That is the
+reason why we are having those tests.
+"""
 import logging
 from typing import cast
 
@@ -5,21 +16,21 @@ import jsonschema
 import pytest
 
 from keboola_mcp_server.client import JsonDict, KeboolaClient
-from keboola_mcp_server.tools._validate import validate_parameters
+from keboola_mcp_server.tools._validate import KeboolaParametersValidator
 from keboola_mcp_server.tools.components.model import Component
 
 LOG = logging.getLogger(__name__)
 
 
-def _safe_validate_parameters(schema: JsonDict, dummy_parameters: JsonDict) -> JsonDict:
-    """
-    Validate parameters against schema but
-    """
+def _check_schema(schema: JsonDict, dummy_parameters: JsonDict) -> None:
     try:
-        return cast(JsonDict, validate_parameters(dummy_parameters, schema)['parameters'])
+        KeboolaParametersValidator.validate(dummy_parameters, schema)
     except jsonschema.ValidationError:
-        # we care only about schema errors, ignore validation errors since we are using dummy parameters
-        return {}
+        # We care only about schema errors, ignore validation errors since we are using dummy parameters.
+        # The schema itself is checked just before we validate json object against it. Hence, we can ingore
+        # ValidationError because the schema is valid which is our objective, but our dummy_parameters violates
+        # the schema - we are not interested in the dummy parameters.
+        pass
 
 
 @pytest.mark.asyncio
@@ -29,26 +40,21 @@ async def test_validate_parameters(keboola_client: KeboolaClient):
     components = cast(list[JsonDict], data['components'])
     components = sorted(components, key=lambda x: (x['type'], x['name']))  # sort by type and then by name
     LOG.info(f'Fetched total of {len(components)} components')
-
     row_counts, root_counts = 0, 0
     for raw_component in components:
-        component = Component.model_validate(raw_component)
-        configuration_schema = component.configuration_schema
-        configuration_row_schema = component.configuration_row_schema
-        if configuration_schema:
-            root_counts += 1
-            validated = _safe_validate_parameters(configuration_schema, dummy_parameters={})
-            assert (
-                validated == {}
-            ), f'{component.component_id} has invalid configuration schema'  # expect returned dummy parameters
-        if configuration_row_schema:
-            row_counts += 1
-            validated = _safe_validate_parameters(configuration_row_schema, dummy_parameters={})
-            assert (
-                validated == {}
-            ), f'{component.component_id} has invalid configuration row schema'  # expect returned dummy parameters
-    # assert False
+        try:
+            component = Component.model_validate(raw_component)
+            configuration_schema = component.configuration_schema
+            configuration_row_schema = component.configuration_row_schema
+            if configuration_schema:
+                root_counts += 1
+                _check_schema(configuration_schema, dummy_parameters={})
+            if configuration_row_schema:
+                row_counts += 1
+                _check_schema(configuration_row_schema, dummy_parameters={})
+        except jsonschema.SchemaError as e:
+            pytest.fail(f'Schema error for {raw_component["id"]}: {e}')
     LOG.info(
-        f'Total components: {len(components)}, from which {root_counts} have root configuration and {row_counts} '
-        'have row configuration'
+        f'Total components: {len(components)}, from which {root_counts} have root configuration schema and {row_counts} '
+        'have row configuration schema. All schemas are valid.'
     )
