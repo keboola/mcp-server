cef4f676fea7b84fa7cead2178c21fe58e86d109
Tomáš Fejfar <tomas.fejfar@gmail.com>
2025-06-19 23:15:10 -0700

    Fix relative import in client.py - use absolute import for KeboolaHTTPException

--- src/keboola_mcp_server/client.py
+++ src/keboola_mcp_server/client.py
@@ -8,7 +8,7 @@ from typing import Any, Literal, Mapping, Optional, Union, cast
 import httpx
 from pydantic import BaseModel, Field
 
-from .errors import KeboolaHTTPException
+from keboola_mcp_server.errors import KeboolaHTTPException
 
 LOG = logging.getLogger(__name__)
 
