f75d63e790b8a7471a1bc598f08b769f63e63985
Nicholas Capek <capekn@centrum.cz>
2025-07-08 22:28:46 +0200

    fix circular import issue

--- src/keboola_mcp_server/tools/components/__init__.py
+++ src/keboola_mcp_server/tools/components/__init__.py
@@ -1,20 +1,8 @@
-from keboola_mcp_server.tools.components.domain_models import (
+from keboola_mcp_server.tools.components.model import (
     Component,
     ComponentConfigurationResponse,
     ComponentConfigurationResponseBase,
     ComponentSummary,
     ComponentType,
     ComponentWithConfigurations,
-)
-from keboola_mcp_server.tools.components.tools import (
-    add_component_tools,
-    add_config_row,
-    create_config,
-    create_sql_transformation,
-    get_config,
-    list_configs,
-    list_transformations,
-    update_config,
-    update_config_row,
-    update_sql_transformation,
 )
